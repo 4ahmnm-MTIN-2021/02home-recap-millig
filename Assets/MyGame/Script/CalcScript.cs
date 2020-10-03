0d2124d87cf9ce46b8cecc0e35dc2e5420888bf6
Melissa Illig <melissaillig@melissas-mbp.home>
2020-10-03 12:17:43 +0200

    create CalcScript

---
+++ Assets/MyGame/Script/CalcScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CalcScript : MonoBehaviour
+{
+    public InputField varA;
+    public InputField varB;
+    public Text TextResult;
+    public Button ButtonAdd;
+
+    public void GetResult()
+    {
+        TextResult.text = AddNumbers().ToString();
+
+    }
+
+
+    private int AddNumbers()
+    {
+        int tempResult = int.Parse(varA.text) + int.Parse(varB.text);
+        return tempResult;
+
+    }
+}
